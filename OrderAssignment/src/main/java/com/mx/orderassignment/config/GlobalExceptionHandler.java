cdb1d9c024c909bed905bb42a425972d2cd3a779
ArthurFig <r2r882@gmail.com>
2025-08-03 14:01:10 -0600

    Implementacion de seguridad en el servicio de OrderAssigment

---
+++ OrderAssignment/src/main/java/com/mx/orderassignment/config/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.mx.orderassignment.config;
+
+import com.mx.orderassignment.dto.Respuesta;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Respuesta handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        return new Respuesta(errorMessage, false, null, LocalDateTime.now());
+    }
+}
